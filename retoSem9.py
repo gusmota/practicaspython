df943940f982bb9450cb0d80b38ea0ddb553780e
hernestorr96 <71956477+hernestorr96@users.noreply.github.com>
2022-08-19 09:27:19 -0500

    First commit

--- retoSem9.py
+++ retoSem9.py
@@ -1,3 +1,11 @@
+#El programa que desarrollarás debe tener las siguientes características: \
+# 1. Que en un bucle infinito solicite al usuario una letra (debes especificar \
+# al usuario la condición para terminar el programa. Por ejemplo, para salir, \
+# escriba alto, presione 0 o cualquier otra que se te ocurra).2. Harás una función \
+# que imprima en la pantalla la letra siguiente en el alfabeto y la letra anterior\
+#  a la ingresada.3. El programa debe continuar en el bucle hasta que el usuario \
+# decida salir del programa
+
 resp =True
 while resp != 0:
    entrada = input('dime una letra: ')
